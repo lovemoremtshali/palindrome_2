303548b1402bd0977622f13d0d1e095544667488
A252460 <A252460@LSBSA-416QCS3>
2024-01-13 09:27:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int reversedNumber = 0, remainder, originalNumber = number;
+
+            while (number != 0)
+            {
+                remainder = number % 10;
+                reversedNumber = reversedNumber * 10 + remainder;
+                number /= 10;
+            }
+
+            if (originalNumber == reversedNumber)
+            {
+                Console.WriteLine("The number {0} is a palindrome.", originalNumber);
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is not a palindrome.", originalNumber);
+            }
+            Console.ReadLine();
+        }
+    }
+}
